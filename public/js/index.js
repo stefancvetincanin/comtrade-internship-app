b4d872cca03b1d729e8fea9f70eede688bac9ad6
toshew <vladantosev@yahoo.com>
2019-06-14 13:19:41 +0200

    Added login function

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,27 @@
-console.log('Radi')
+let user = JSON.parse(sessionStorage.getItem('loggedUser'));
+
+if (user) {
+    $('#btnLogin').addClass('d-none');
+    $('#btnLogout').addClass('d-inline-block');
+    $('#loggedUser').text(user[0].ime);
+    $('#loginModal').modal('hide');
+    $( "#search" ).prop( "disabled", false );
+}
+
+document.getElementById('loginBtn').addEventListener('click', login);
+
+function login() {
+    const username = document.getElementById('user').value;
+    const password = document.getElementById('pass').value;
+    
+    fetch(`/login/${username}/${password}`)
+            .then(res => res.json())
+            .then(res => {
+                sessionStorage.setItem('loggedUser', JSON.stringify(res));
+                $('#btnLogin').addClass('d-none');
+                $('#btnLogout').addClass('d-inline-block');
+                $('#loggedUser').text(res[0].ime);
+                $( "#search" ).prop( "disabled", false );
+            });
+}
+
