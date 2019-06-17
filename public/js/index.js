b9042b70fa30efa316ed391551fd7461c47cddd1
toshew <vladantosev@yahoo.com>
2019-06-17 12:41:08 +0200

    Added dynamic search for cities

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,3 @@
-
-let display = '';
-fetch('/cities')
-  .then(res => res.json())
-  .then(res => {
-      res.forEach(element => {
-        display += `<li class="list-group-item list-group-item-primary mb-1">
-                <a href="city.html?grad-id=${element.id}">
-                  <div class="row align-items-center text-center">
-                      <div class="col-lg-2 col-md-3 col-sm-4">
-                          <img class="d-block mx-auto" src="${element.slika}" height="100px" alt="${element.ime}">
-                      </div>
-                      <div class="col-lg-9 col-md-8 ml-3 col-sm-7">
-                          <h1 class="text-center">${element.ime}</h1>
-                          <p id="descriptionCity" class="text-left">${element.opis}</p>
-                      </div>
-                  </div>   
-                </a>     
-              </li>`
-              document.getElementById('cityList').innerHTML = display;
-      });
-  });
-
-  
-
 let user = JSON.parse(sessionStorage.getItem('loggedUser'));
 
 if (user) {
@@ -31,9 +6,11 @@ if (user) {
     $('#loggedUser').text(user[0].ime);
     $('#loginModal').modal('hide');
     $( "#search" ).prop( "disabled", false );
+    getCities();
 }
 
 document.getElementById('loginBtn').addEventListener('click', login);
+document.getElementById('search').addEventListener('keyup', filterCities);
 
 function login() {
     const username = document.getElementById('user').value;
@@ -47,6 +24,39 @@ function login() {
                 $('#btnLogout').addClass('d-inline-block');
                 $('#loggedUser').text(res[0].ime);
                 $( "#search" ).prop( "disabled", false );
+                getCities();
             });
 }
 
+let cityArr;
+
+function getCities() {
+    fetch('/cities')
+        .then(res => res.json())
+        .then(data => {
+            cityArr = data;
+            $('#cityList').html(data.map(cityTemplate).join(''));
+        });
+}
+
+const cityTemplate = element => `
+<li class="list-group-item list-group-item-primary mb-1">
+    <a href="city.html?grad-id=${element.id}">
+        <div class="row align-items-center text-center">
+            <div class="col-lg-2 col-md-3 col-sm-4">
+                <img class="d-block mx-auto" src="${element.slika}" height="100px" alt="${element.ime}">
+            </div>
+            <div class="col-lg-9 col-md-8 ml-3 col-sm-7">
+                <h1 class="text-center">${element.ime}</h1>
+                <p id="descriptionCity" class="text-left">${element.opis}</p>
+            </div>
+        </div>   
+    </a>     
+</li>`;
+ 
+function filterCities(e) {
+    $('#cityList').html(cityArr
+            .filter(element => element.ime.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1)
+            .map(cityTemplate)
+            .join(''));
+}
