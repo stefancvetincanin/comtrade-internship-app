2453a275ac46b89cbd0234207f96228c40b70508
lazarvelickovic <lazarvtst@gmail.com>
2019-06-18 12:33:04 +0200

    Making changes on accommodation.html file and city.js file and adding new accomodation.js file

--- public/js/accommodation.js
+++ public/js/accommodation.js
@@ -26,17 +26,17 @@ let idHotel = getParameter("hotel-id");
 // prikaz imena grada
 fetch(`/city/${idGrada}`)
   .then(res => res.json())
-  .then(res => (document.getElementById("city-name").innerHTML = `${res[0].ime}`));
+  .then(res => document.getElementById("city-name").innerHTML = `${res[0].ime}`);
 
 // prikaz imena hotela i opisa
-fetch(`/hotels/${idHotel}`)
+fetch(`/hotel/${idHotel}`)
 .then(res => res.json())
 .then(
-  res => 
-    (document.getElementById(
-      "hotel-description"
-    ).innerHTML = `<h1 class="h-25">${res[0].ime}</h1>
+  res => {
+    document.getElementById("hotel-description").innerHTML = 
+    `<h1 class="h-25">${res[0].ime}</h1>
     <div id="starsAccomod" class="align-items-center">*****</div>
     <p class="h-75 pr-3">${res[0].opis}</p>
-    <button class="btn btn-primary d-inline-block mb-3" type="button" data-toggle="modal" data-target="#addFeed">Dodaj feed...</button>`)   
+    <button class="btn btn-primary d-inline-block mb-3" type="button" data-toggle="modal" data-target="#addFeed">Dodaj feed...</button>`
+  }
 );
